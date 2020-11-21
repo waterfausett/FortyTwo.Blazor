82e41d3f24f6e159252459089c1ba63adbcdd0c1
Adam Fausett <waterfausett@gmail.com>
2020-11-21 00:27:55 -0600

    wip

---
+++ FortyTwo/Client/ViewModels/DominoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FortyTwo.Shared.Models;
+
+namespace FortyTwo.Client.ViewModels
+{
+    public interface IDominoModel
+    {
+        Task<Domino[]> ShuffleAsync();
+    }
+
+    public class DominoViewModel : IDominoModel
+    {
+        private HttpClient _http;
+
+        public DominoViewModel(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<Domino[]> ShuffleAsync()
+        {
+            var response = await _http.GetAsync("Dominos");
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            //return JsonSerializer.Deserialize<Domino[]>(responseContent, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            return new Domino[] { new Domino { Top = 1, Bottom = 0 } };
+        }
+    }
+}
