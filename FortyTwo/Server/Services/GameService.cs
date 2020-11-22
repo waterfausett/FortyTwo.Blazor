e5a2db95393a1d9068d25db082b8d22c94564b5c
Adam Fausett <waterfausett@gmail.com>
2020-11-22 00:38:28 -0600

    wip - temp "persistence"

--- FortyTwo/Server/Services/GameService.cs
+++ FortyTwo/Server/Services/GameService.cs
@@ -15,14 +15,77 @@ namespace FortyTwo.Server.Services
 
     public class GameService : IGameService
     {
-        private readonly List<Domino> _dominos;
-
-        public GameService()
+        public async Task<List<GameContext>> FetchGamesAsync()
         {
-            _dominos = InitDominos();
+            return StaticGames.Instance;
         }
 
-        private List<Domino> InitDominos()
+        public async Task<GameContext> GetAsync(Guid id)
+        {
+            return (await FetchGamesAsync())[0];
+        }
+    }
+
+    // TODO: temporary - will remove when we have persistance
+    public class StaticGames
+    {
+        private static List<GameContext> _games;
+
+        public static List<GameContext> Instance
+        {
+            get
+            {
+                if (_games == null)
+                {
+                    var dominos = InitDominos();
+                    _games = new List<GameContext>
+                    {
+                        new GameContext
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Game 1",
+                            Players = new List<Player>
+                            {
+                                new Player
+                                {
+                                    Id = "Id:Luke",
+                                    Name = "Luke",
+                                    TeamId = new Guid("a58f497f-73fe-4a8b-9af4-a409ca385c66"),
+                                    IsActive = true,
+                                    Dominos = dominos.GetRange(0, 7)
+                                },
+                                new Player
+                                {
+                                    Id = "Id:Adam",
+                                    Name = "Adam",
+                                    TeamId = new Guid("bca0607d-013a-410d-bac2-90ce4fd78bfa"),
+                                    Dominos = dominos.GetRange(7, 7)
+                                },
+                                new Player
+                                {
+                                    Id = "Id:Sam",
+                                    Name = "Sam",
+                                    TeamId = new Guid("a58f497f-73fe-4a8b-9af4-a409ca385c66"),
+                                    Dominos = dominos.GetRange(14, 7)
+                                },
+                                new Player
+                                {
+                                    Id = "Id:Emily",
+                                    Name = "Emily",
+                                    TeamId = new Guid("bca0607d-013a-410d-bac2-90ce4fd78bfa"),
+                                    Dominos = dominos.GetRange(21, 7)
+                                },
+                            },
+                        }
+                    };
+
+                }
+
+                return _games;
+            }
+        }
+
+        private static List<Domino> InitDominos()
         {
             var dominos = new List<Domino>();
             for (var i = 0; i < 7; ++i)
@@ -36,55 +99,6 @@ namespace FortyTwo.Server.Services
             dominos.Shuffle();
 
             return dominos;
-        }
-
-        public async Task<List<GameContext>> FetchGamesAsync()
-        {
-            return new List<GameContext>
-            {
-                new GameContext
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Game 1",
-                    Players = new List<Player>
-                    {
-                        new Player
-                        {
-                            Id = "Id:Luke",
-                            Name = "Luke",
-                            TeamId = new Guid("a58f497f-73fe-4a8b-9af4-a409ca385c66"),
-                            IsActive = true,
-                            Dominos = _dominos.GetRange(0, 7)
-                        },
-                        new Player
-                        {
-                            Id = "Id:Adam",
-                            Name = "Adam",
-                            TeamId = new Guid("bca0607d-013a-410d-bac2-90ce4fd78bfa"),
-                            Dominos = _dominos.GetRange(7, 7)
-                        },
-                        new Player
-                        {
-                            Id = "Id:Sam",
-                            Name = "Sam",
-                            TeamId = new Guid("a58f497f-73fe-4a8b-9af4-a409ca385c66"),
-                            Dominos = _dominos.GetRange(14, 7)
-                        },
-                        new Player
-                        {
-                            Id = "Id:Emily",
-                            Name = "Emily",
-                            TeamId = new Guid("bca0607d-013a-410d-bac2-90ce4fd78bfa"),
-                            Dominos = _dominos.GetRange(21, 7)
-                        },
-                    },
-                }
-            };
-        }
-
-        public async Task<GameContext> GetAsync(Guid id)
-        {
-            return (await FetchGamesAsync())[0];
         }
     }
 }
