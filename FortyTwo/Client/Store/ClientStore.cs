ee742657f5f40af702485a9c42055af15397e71d
Adam Fausett <waterfausett@gmail.com>
2020-11-21 23:53:15 -0600

    wip

--- FortyTwo/Client/Store/ClientStore.cs
+++ FortyTwo/Client/Store/ClientStore.cs
@@ -12,7 +12,7 @@ namespace FortyTwo.Client.Store
 
     public class ClientStore : IClientStore
     {
-        public List<Domino> Dominos { get; set; }
-        public List<Game> Games { get; set; }
+        public List<Domino> Dominos { get; set; } = new List<Domino>();
+        public List<Game> Games { get; set; } = new List<Game>();
     }
 }
