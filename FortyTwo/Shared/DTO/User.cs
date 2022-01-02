31c396744e7d5f85ca840a2f598a7e379a740240
Adam Fausett <waterfausett@gmail.com>
2022-01-01 20:44:49 -0600

    temporarily show email instead of nickname for easier testing

--- FortyTwo/Shared/DTO/User.cs
+++ FortyTwo/Shared/DTO/User.cs
@@ -16,7 +16,7 @@ namespace FortyTwo.Shared.DTO
         public string Picture { get; set; }
 
         public string DisplayName
-            => Nickname
+            => Email// HACK: testing //Nickname
                 ?? ($"{FirstName} {(!string.IsNullOrWhiteSpace(LastName) ? $"{LastName?.Substring(0, 1)}." : "")}")
                 ?? Email
                 ?? $"Unknown User ({Id})";
