8e7e6bc089f9764251acba1313add8a44b11cbd0
Adam Fausett <waterfausett@gmail.com>
2022-01-10 19:10:27 -0600

    beginnings of support for dark theme and system OS setting support

--- FortyTwo/Client/wwwroot/js/app.js
+++ FortyTwo/Client/wwwroot/js/app.js
@@ -1,3 +1,17 @@
 ﻿async function swalAlert(options) {
     return Swal.fire(options);
 }
+
+$(() => {
+    return;
+
+    // TODO: support theme options in /profile
+    //  - use selected mode regardless of OS level setting
+
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefersDarkScheme.matches) {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+});
